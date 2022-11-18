8929cb6eda74dfd4c0d9432472eacff2562ea501
Yahya <87653635+Y7EA@users.noreply.github.com>
2022-11-18 17:53:29 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,28 +13,6 @@ typedef unsigned long   DWORD;  //4 bytes (0~2^32 -1)
 
 struct INFO
 {
-    // BIT MAP FILE HEADER (14 bytes) from 16 reducing to 14
-    /* Bitmap file format
-    *
-    * SECTION
-    * Address:Bytes	Name
-    *
-    * HEADER:
-    *	  0:	2		"BM" magic number
-    *	  2:	4		file size
-    *	  6:	4		junk
-    *	 10:	4		Starting address of image data
-    * BITMAP HEADER:
-    *	 14:	4		header size
-    *	 18:	4		width  (signed)
-    *	 22:	4		height (signed)
-    *	 26:	2		Number of color planes
-    *	 28:	2		Bits per pixel
-    *	[...]
-    * [OPTIONAL COLOR PALETTE, NOT PRESENT IN 32 BIT BITMAPS]
-    * BITMAP DATA:
-    *	138:	X	Pixels
-    */
     WORD bfType;          //BM -> 0x4d42 (19778)
     DWORD BfSize;
     WORD bfReserved1;
@@ -77,7 +55,7 @@ public:
     {
         this->height=height;
         this->width=width;
-        rowsize=(3*width+3)/4*4;   //set to be a multiple of "4"
+        rowsize=(3*width+3);
         term=new BYTE[height*rowsize];
         for(int y=0; y<height; y++)
             for(int x=0; x<width; x++)
@@ -106,7 +84,7 @@ public:
         INFO h=
         {
             19778,   //0x4d42
-            DWORD(54+rowsize*height),
+            DWORD(rowsize*height),
             0,
             0,
             54,
@@ -117,8 +95,8 @@ public:
             24,
             0,
             DWORD(rowsize*height),
-            3780,   //3780
-            3780,   //3780
+            72,
+            72,
             0,
             0
         };
@@ -133,7 +111,7 @@ public:
 
 
  // Variables
-    long lIDXA = 0;
+    long INDEX = 0;
     double byteRGB_RA = 0;
     double byteRGB_GA = 0;
     double byteRGB_BA = 0;
@@ -149,16 +127,16 @@ Image grayscale(Image input){
     {
         for ( int iX = 0; iX < input.width; iX++ )
         {
-            lIDXA = ( iX *input.height* 3 ) + ( iY * 3 );
-            byteRGB_BA =  input.term[lIDXA+0]; //Blue
-            byteRGB_GA =  input.term[lIDXA+1]; //Green
-            byteRGB_RA =  input.term[lIDXA+2]; //Red
+            INDEX = ( iX *input.height* 3 ) + ( iY * 3 );
+            byteRGB_BA =  input.term[INDEX+0]; //Blue
+            byteRGB_GA =  input.term[INDEX+1]; //Green
+            byteRGB_RA =  input.term[INDEX+2]; //Red
             // To transform RGB to  Gray scale).
             resRGB =  ( byteRGB_RA +  byteRGB_GA +  byteRGB_BA)/3;
 
-            input.term[lIDXA+0] = resRGB;
-            input.term[lIDXA+1] = resRGB;
-            input.term[lIDXA+2] = resRGB;
+            input.term[INDEX+0] = resRGB;
+            input.term[INDEX+1] = resRGB;
+            input.term[INDEX+2] = resRGB;
         }
     }
 
@@ -184,8 +162,8 @@ Image dithering(Image input)
                 for ( int j = 0; j < 2; j++ )
                 {
                    // Read The index of pixel so mult 3
-                    lIDXA = ((x+i)*3)+((y+j)*input.width* 3 );
-                    Gray = input.term[lIDXA+0];
+                    INDEX = ((x+i)*3)+((y+j)*input.width* 3 );
+                    Gray = input.term[INDEX+0];
                     if ( Gray > orderedDithering  [ i ][ j ] )
                     {
                         outDith = 255;
@@ -195,9 +173,9 @@ Image dithering(Image input)
                         outDith = 0;
                     }
                     // Half-tone image
-                    input.term[lIDXA+0] = outDith;
-                    input.term[lIDXA+1] = outDith;
-                    input.term[lIDXA+2] = outDith;
+                    input.term[INDEX+0] = outDith;
+                    input.term[INDEX+1] = outDith;
+                    input.term[INDEX+2] = outDith;
                 }
             }
         }
@@ -219,6 +197,31 @@ int main()
     output = grayscale (input );
     output=dithering(input );
     output.save("rahafssss.png");
-    cout<<"Done";
+   cout << "                     ,---.           ,---.   " << endl;
+	cout << "                    / /^`.\\.--''''-./,'^\\ \\	" << endl;
+	cout << "                    \\ \\    _       _    / /	" << endl;
+	cout << "                     `./  / __   __ \\  \\,'	" << endl;
+	cout << "                      /    /_O)_(_O\\    \\	" << endl;
+	cout << "                      |  .-'  ___  `-.  |	" << endl;
+	cout << "                   .--|       \\_/       |--.	" << endl;
+	cout << "                 ,'    \\   \\   |   /   /    `." << endl;
+	cout << "                /       `.  `--^--'  ,'       \\" << endl;
+	cout << "             .-^^^^^-.    `--.___.--'     .-^^^^^-." << endl;
+	cout << ".-----------/         \\------------------/         \\--------------." << endl;
+	cout << "| .---------\\         /----------------- \\         /------------. |" << endl;
+	cout << "| |          `-`--`--'                    `--'--'-'             | |" << endl;
+	cout << "| |                                                             | |" << endl;
+	cout << "| |                            Done                             | |" << endl;
+	cout << "| |                                                             | |" << endl;
+	cout << "| |                                                             | |" << endl;
+	cout << "| |_____________________________________________________________| |" << endl;
+	cout << "|_________________________________________________________________|" << endl;
+	cout << "                   )__________|__|__________(" << endl;
+	cout << "                  |            ||            |" << endl;
+	cout << "                  |____________||____________|" << endl;
+	cout << "                    ),-----.(      ),-----.(" << endl;
+	cout << "                  ,'   ==.   \\    /  .==    `." << endl;
+	cout << "                 /            )  (            \\" << endl;
+	cout << "                 `==========='    `==========='  " << endl;
 
 }
